647bcad44a535fdb59e1fe9b69a9beed0eb22fc1
Adam Shaw <adam@kabukivision.com>
2019-02-06 09:40:31 -0500

    Initial Commit

---
+++ FirestoreLargeWriteTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirestoreLargeWriteTest
+//
+//  Created by Adam Shaw on 2/6/19.
+//  Copyright © 2019 Test Org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
